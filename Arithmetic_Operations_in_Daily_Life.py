af9ce7f53b29968aa19a749a1f85312a9c700da8
gabegodoi <gabegodoi@gmail.com>
2024-07-31 23:25:21 -0700

    removed decimal library

--- Arithmetic_Operations_in_Daily_Life.py
+++ Arithmetic_Operations_in_Daily_Life.py
@@ -15,8 +15,6 @@ print(f"the total cost of bread, peanut butter, & jelly is ${bread_cost + peanut
 # So if you had $10,000 at a 7% interest write code that would tell us the amount at the end of the year. 
 # For the example the expected outcome would be $10,700.
 
-from decimal import Decimal
-
 savings_account_balance = 10000
 interest_rate = 0.07 / 365
 year = 365
